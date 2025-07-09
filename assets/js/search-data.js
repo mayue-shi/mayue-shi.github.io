634334d077a3bf407ef587a4ebd97f5f4dfac633
mayue-shi <73474749+mayue-shi@users.noreply.github.com>
2025-07-09 18:10:25 +0000

    Deploying to gh-pages from @ mayue-shi/mayue-shi.github.io@4435edcfdf61df06f71ea1cab4920eb17deb6101 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,7 +58,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/news/";
           },
-        },{id: "books-the-godfather",
+        },{id: "post-a-reflection",
+        
+          title: "A reflection",
+        
+        description: "this is what included images could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2015/images/";
+          
+        },
+      },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
