88d74069250e506230ca91afdb7e127cd9c6f93b
mayue-shi <73474749+mayue-shi@users.noreply.github.com>
2025-08-14 22:03:20 +0000

    Deploying to gh-pages from @ mayue-shi/mayue-shi.github.io@28aecff53d72effbad9096cd14d16e9a960c5c2f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -83,8 +83,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_20250705/";
-            },},{id: "news-happy-to-give-a-talk-at-the-institute-of-biomedical-engineering-at-oxford",
-          title: 'Happy to give a talk at the Institute of Biomedical Engineering at Oxford...',
+            },},{id: "news-dr-shi-gave-a-talk-at-the-institute-of-biomedical-engineering-at-oxford",
+          title: 'Dr Shi gave a talk at the Institute of Biomedical Engineering at Oxford...',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_20250801/";
